1204294446cd0b1325c48699398523973e2d6f63
Ali Rostami <ali.c.rostami@gmail.com>
2021-07-10 11:37:20 +0430

    Add support for moving crosshair

--- src/gui/mouse-event-handler.ts
+++ src/gui/mouse-event-handler.ts
@@ -23,6 +23,11 @@ export interface MouseEventHandlers {
 	longTapEvent?: HandlerEventCallback;
 }
 
+export interface TouchMouseEventLocal {
+	readonly localX: number;
+	readonly localY: number;
+}
+
 export interface TouchMouseEvent {
 	readonly clientX: number;
 	readonly clientY: number;
